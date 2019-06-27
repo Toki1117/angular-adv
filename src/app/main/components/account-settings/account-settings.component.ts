dc6c2a71c84a9f52a10f3851b770bff0a61b6c54
Toki1117 <diana.salguero11@outlook.com>
2019-06-26 18:25:22 -0600

    :construction: adding check to selected theme

--- src/app/main/components/account-settings/account-settings.component.ts
+++ src/app/main/components/account-settings/account-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ElementRef } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -15,10 +15,19 @@ export class AccountSettingsComponent implements OnInit {
 
   cambiarColor(event) {
     if ( event.target.tagName === 'A' ) {
-      console.log(event.target.dataset.theme );
       const tema = event.target.dataset.theme;
       const urlStyle = `assets/css/colors/${ tema }.css`;
       this.document.getElementById('theme').setAttribute('href', urlStyle);
+
+      const selectores: Array<HTMLElement> = this.document.getElementsByClassName('selector');
+
+      for ( const selector of selectores ) {
+        selector.classList.remove('working');
+      }
+
+      event.target.classList.add('working');
+
+      console.log(event.target.dataset.theme, selectores );
     }
   }
 
